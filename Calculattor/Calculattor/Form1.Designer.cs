02dc81b7d847cc80a45ea5f34a1a7c1e97d4ce8f
arriinnna <152606000+arriinnna@users.noreply.github.com>
2023-12-01 14:12:12 +0300

    Add files via upload

---
+++ Calculattor/Calculattor/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Calculattor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ageNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(12, 76);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(163, 20);
+            this.heightTextBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите рост";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(14, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите вес";
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Location = new System.Drawing.Point(12, 167);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(163, 20);
+            this.weightTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(270, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите возраст";
+            // 
+            // ageNumericUpDown
+            // 
+            this.ageNumericUpDown.Location = new System.Drawing.Point(275, 76);
+            this.ageNumericUpDown.Name = "ageNumericUpDown";
+            this.ageNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.ageNumericUpDown.TabIndex = 6;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(275, 164);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 9;
+            this.calculateButton.Text = "OK";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(81, 293);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(132, 29);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.ageNumericUpDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.weightTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.heightTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown ageNumericUpDown;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Calculattor/Calculattor/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculattor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+          // метод для форм в дизайнере форм VS
+          InitializeComponent();
+        }
+        // Метод,  будет вызываться на кнопку клика "ОК"
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // Получаем значения роста, веса и возраста из форм куда пользователь писал данные
+            double height = double.Parse(heightTextBox.Text);
+            double weight = double.Parse(weightTextBox.Text);
+            int age = int.Parse(ageNumericUpDown.Text);
+
+            // Делаю ограничения
+            // Рост должен быть больше 145 и меньше 210
+            if (height < 145 || height > 210)
+            {
+                MessageBox.Show("Рост должен быть больше 145 и меньше 210");
+                return;
+            }
+
+            // Вес должен быть больше 40 и меньше 200
+            if (weight < 40 || weight > 200)
+            {
+                MessageBox.Show("Вес должен быть больше 40 и меньше 200");
+                return;
+            }
+
+            // Возраст должен быть больше 12 и меньше 100
+            if (age < 12 || age > 100)
+            {
+                MessageBox.Show("Возраст должен быть больше 12 и меньше 100");
+                return;
+            }
+
+            // Расчет калорийности на основе роста, веса и возраста
+            double calorieResult = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
+
+            // Вывод результата расчета на форму
+            resultLabel.Text = $"Ваша суточная норма калорийности: {calorieResult} калорий";
+        }
+
+        
+    }
+}
